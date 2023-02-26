eb470a767cbae00555dcc590b6cbb8ecb8cfef8f
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-26 15:24:34 +0100

    modify files

--- src/components/interests.js
+++ src/components/interests.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export default function Interest(){
+return(
+  <div>
+<h3>Interest</h3>
+<p> Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+  </div>
+)
+}
