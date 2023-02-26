bd88fbe65fcb85e8f89f9b45ae3a657e803b7220
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-26 15:26:35 +0100

    modify info.js
    fix bugs

--- src/components/info.js
+++ src/components/info.js
@@ -1,13 +1,13 @@
 import React from "react";
 import avatar from "../images/Rectangle 90.png";
 import "../App.css";
-function Info() {
+export default function Info() {
   return (
     <div>
       <img src={avatar} alt="avatar" />
       <h2>Laura Smith</h2>
       <h4>Frontend Developer</h4>
       <p>larausmith</p>
-    </div>
+    </div> 
   );
 }
