1c42bb9a9d83f17f365a15f8f9a533e41db16223
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-26 15:42:23 +0100

    modify info.js

--- src/components/info.js
+++ src/components/info.js
@@ -1,5 +1,7 @@
 import React from "react";
 import avatar from "../images/Rectangle 90.png";
+import mail from "../images/Mail.png";
+import link from "../images/linkedin.png";
 import "../App.css";
 export default function Info() {
   return (
@@ -8,6 +10,10 @@ export default function Info() {
       <h2>Laura Smith</h2>
       <h4>Frontend Developer</h4>
       <p>larausmith</p>
+      <div>
+        <a href="heelo"><img src={mail} alt=""></img> Email</a>
+        <a href="heelo"><img src={link} alt=""></img> linkedIn</a>
+      </div>
     </div> 
   );
 }
